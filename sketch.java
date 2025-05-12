57c36617af3c084466bdd783e0f7d5ad7ea30268
FemboyFireball <111195856+femboyfireball@users.noreply.github.com>
2025-05-11 19:12:09 -0500

    Add entity classes, remove JavaDoc, and more
    Added the entity, player, and enemy classes
    Removed all JavaDoc from the code temporarily
    Refactored collision code to be simpler
    Set up the rendering code
    Add the level platforms (Theoretically at least, it's broken rn)

--- sketch.java
+++ sketch.java
@@ -1,128 +1,150 @@
-import java.lang.reflect.Constructor;
 import java.util.*;
 
+class Entity {
 
-class Player {
+    int positionX;
+    int positionY;
 
-    public int xPosition;
-    public int yPosition;
-    public int xVelocity;
-    public int yVelocity;
+    int velocityX;
+    int velocityY;
 
-    public int bBoxWidth;
-    public int bBoxHeight;
-    
-    
+    int maxVelocityX;
+    int maxVelocityY;
+
+    int boundBodWidth;
+    int boundBoxHeight;
+
+    int[] color;
 }
 
-/**
- * Stores information about a platform
- * @author Daniel K
- */
+class Player extends Entity {
+    int level;
+    int health;
+
+    boolean isAirborne;
+    boolean isAttacking;
+
+    Player() {
+        this.level = 0;
+        this.health = 100;
+        this.isAirborne = false;
+        this.isAttacking = false;
+        
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.maxVelocityX = 100;
+        this.maxVelocityY = 100;
+        this.boundBodWidth = 50;
+        this.boundBoxHeight = 100;
+
+        this.color = new int[] { 20, 140, 200 };
+    }
+}
+
+class Enemy extends Entity {
+    String target;
+}
+
 class Platform {
     public int[]  platformCoordinates;
     public String platformType;
     public int[]  platformColor;
 
-    /**
-     * Creates the platform
-     * @param coordinates Coordinates (Top left and bottom right corners)
-     * @param type Type of the platform (Normal, Sticky, ect)
-     * @param color Color of the platform (R, G, B)
-     */
-    Platform(int[] coordinates, String type, int[] color) {
+    Platform(int[] coordinates, String type, int[] platformColor) {
         this.platformCoordinates = coordinates;
         this.platformType = type;
-        this.platformColor = color;
+        this.platformColor = platformColor;
     }
 
-    /**
-     * Detects if the player is colliding with a platform
-     * @param playerX
-     * @param playerY
-     * @param playerWidth
-     * @param playerHeight
-     * @return A boolean[] representing which edge(s) are being collidded with. 0 (None), 1 (Top), 2 (Right), 3 (Bottom), 4 (Left)
-     */
     boolean[] playerColliding(int playerX, int playerY, int playerWidth, int playerHeight) {
-
         boolean[] edgesColliding = new boolean[5];
-        edgesColliding[0] = true;
         Arrays.fill(edgesColliding, false);
 
-        if (playerY > this.platformCoordinates[1] && playerY < this.platformCoordinates[2]) {
-            edgesColliding[0] = false;
-            edgesColliding[1] = true;
-        }
-        if (playerX + playerWidth > this.platformCoordinates[0] && playerX + playerWidth < this.platformCoordinates[3]) {
-            edgesColliding[0] = false;
-            edgesColliding[2] = true;
-        }
-        if (playerY + playerHeight > this.platformCoordinates[1] && playerY + playerHeight < this.platformCoordinates[2]) {
-            edgesColliding[0] = false;
-            edgesColliding[3] = true;
-        }
-        if (playerX > this.platformCoordinates[0] && playerX < this.platformCoordinates[3]) {
-            edgesColliding[0] = false;
-            edgesColliding[4] = true;
+        if (playerY > this.platformCoordinates[1] && playerY < this.platformCoordinates[2]) edgesColliding[1] = true;
+        if (playerX + playerWidth > this.platformCoordinates[0] && playerX + playerWidth < this.platformCoordinates[3]) edgesColliding[2] = true;
+        if (playerY + playerHeight > this.platformCoordinates[1] && playerY + playerHeight < this.platformCoordinates[2]) edgesColliding[3] = true;
+        if (playerX > this.platformCoordinates[0] && playerX < this.platformCoordinates[3]) edgesColliding[4] = true;
+
+        if (!edgesColliding[1] && !edgesColliding[2] && !edgesColliding[3] && !edgesColliding[4]) {
+            edgesColliding[0] = true; // No collision
         }
 
         return edgesColliding;
     }
-
 }
 
-/**
- * Stores information about the current level
- * @author Daniel K
- */
 class Level {
-
-    /**
-     * Array of the platforms in this level
-     */
     public ArrayList<Platform> platforms;
 
-    /**
-     * Create the level
-     * @param platformCount Number of platforms in the level
-     */
     Level(int platformCount) {
         this.platforms = new ArrayList<Platform>(platformCount);
     }
 
-    /**
-     * Adds a new platform to the level
-     * @param platform Index of the platform to add
-     * @param coordinates Coordinates for the platform (Top Left X, Top Left Y, Bottom Left X, Bottom Left Y)
-     * @param type Type of the platform (Normal, Sticky, ect)
-     * @param color Color of the platform (R, G, B)
-     * @author Daniel K
-     */
-    void addPlatform(int platform, int[] coordinates, String type, int[] color) {
-
-        Platform newPlatform = new Platform( coordinates, "Normal", color );
-        platforms.add(platform, newPlatform);
+    void addPlatform(int[] coordinates, String type, int[] platformColor) {
+        platforms.add(new Platform( coordinates, type, platformColor ));
     }
 
     void renderLevel() {
-
-    }
-}
-
-
-
-class Main {
-    public void start() {
-        ArrayList<Level> levels = new ArrayList<Level>();
-        levels.add(new Level(5));
-        levels.add(new Level(6));
-
-        for (Level level : levels) {
-            for (int platform : level.platforms )
+        for (Platform platform : platforms) {
+            fill(platform.platformColor[0], platform.platformColor[1], platform.platformColor[2]);
+            rect(platform.platformCoordinates[0],
+                    platform.platformCoordinates[1],
+                    platform.platformCoordinates[2] - platform.platformCoordinates[0], // Width
+                    platform.platformCoordinates[2] - platform.platformCoordinates[1]  // Height
+            );
         }
     }
 }
 
-Main game = new Main();
-game.start();
+Main game;
+int level = 0;
+
+void setup() {
+    size(1920, 1080); // Or your desired size
+    fullScreen();
+    game = new Main();
+    game.start();
+    
+}
+
+void draw() {
+    background(0); // Clear the background each frame
+    switch (level) {
+      case 0:
+        game.levels.get(0).renderLevel();
+    }
+}
+
+class Main {
+    public ArrayList<Level> levels = new ArrayList<Level>();
+
+    public void start() {
+        levels.add(new Level(5));
+        levels.add(new Level(7));
+
+        int[] greyPlatform = { 100, 100, 100 };
+        int[] icePlatform =  { 10,  200, 240 };
+
+        levels.get(0).addPlatform(new int[] { 0,     900,   1920,  180 }, "Normal", greyPlatform);
+        levels.get(0).addPlatform(new int[] { 240,   720,   360,   36  }, "Normal", greyPlatform);
+        levels.get(0).addPlatform(new int[] { 720,   450,   150,   360 }, "Normal", greyPlatform);
+        levels.get(0).addPlatform(new int[] { 1200,  630,   480,   36  }, "Normal", greyPlatform);
+        levels.get(0).addPlatform(new int[] { 120,   180,   240,   36  }, "Normal", greyPlatform);
+
+        levels.get(1).addPlatform(new int[] { 0,     600,   800,   650  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 50,    450,   200,   470  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 50,    300,   70,    450  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 150,   300,   250,   320  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 300,   400,   500,   420  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 600,   600,   700,   650  }, "Normal", greyPlatform);
+        levels.get(1).addPlatform(new int[] { 0,     800,   200,   820  }, "Ice",    icePlatform );
+    }
+
+    private void mainLoop() {
+        while (true) {
+            // Your main game loop logic would go here
+        }
+    }
+}
